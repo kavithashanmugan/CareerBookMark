16e3ac0b33bf0087e0380335cee562c2ef60453a
kavithashanmugan <kavitha.shanmugan@gmail.com>
2021-06-21 19:45:25 -0600

    styling for search bar in manage-jobs

--- src/app/components/manage-jobs/manage-jobs.component.ts
+++ src/app/components/manage-jobs/manage-jobs.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { ApiService } from "../../shared/api.service";
 @Component({
   selector: 'app-manage-jobs',
   templateUrl: './manage-jobs.component.html',
   styleUrls: ['./manage-jobs.component.css']
 })
 export class ManageJobsComponent implements OnInit {
+  searchMyJobForm = new FormGroup({
+    keywords:new FormControl('',Validators.required)
+  })
+  constructor(private formBuilder: FormBuilder,private router: Router,public api: ApiService,private _Activatedroute:ActivatedRoute) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(){
+    this.searchMyJobForm = this.formBuilder.group({
+      keywords: [''],
+    })
   }
 
 }
